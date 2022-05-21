844f16a8e44b4bbadfb25ce8882edc864881c38d
Nikola Hadjiev <83541964+nikola878@users.noreply.github.com>
2022-05-21 18:04:54 +0300

    Add files via upload

---
+++ PhoneShop/PhoneShop/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PhoneShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> phones = Console.ReadLine()
+                .Split(", ")
+                .ToList();
+            string command = Console.ReadLine();
+
+            while (command != "End")
+            {
+                string[] elements = command.Split(" - ");
+                string command1 = elements[0];
+                string phone = elements[1];
+                switch (command1 )
+                {
+                    case "Add":
+                        for (int i = 0; i < phones.Count; i++)
+                        {
+                            if (phones[i] != phone)
+                            {
+                                phones.Add(phone);
+                            }
+
+                            break;
+                        }
+                        break;
+                    case "Remove":
+                        for (int i = 0; i < phones.Count; i++)
+                        {
+                            if (phones[i] == phone)
+                            {
+                                phones.RemoveAt(i);
+                            }
+
+                            break;
+                            
+                        }
+                        break;
+                    case "Bonus phone":
+                        string[] twoPhones = phone.Split(":");
+                        
+                        for (int i = 0; i < phones.Count; i++)
+                        {
+                            if (phones[i] == twoPhones[0])
+                            {
+                                phones.Add(twoPhones[1]);
+                            }
+
+                            break;
+                        }
+                        break;
+                    case "Last":
+                        for (int i = 0; i < phones.Count; i++)
+                        {
+                            if (phones[i] == phone)
+                            {
+                                phones.RemoveAt(i);
+                                phones.Add(phone);
+                            }
+
+                            break;
+                        }
+                        break;
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(", ", phones));
+        }
+    }
+}
