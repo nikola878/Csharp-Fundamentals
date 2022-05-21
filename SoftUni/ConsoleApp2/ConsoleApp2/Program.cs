1f1e80fde59ad56dfc0ed0a4e79101a782d5a746
Nikola Hadjiev <83541964+nikola878@users.noreply.github.com>
2022-05-21 18:05:34 +0300

    Add files via upload

---
+++ SoftUni/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int brPens = int.Parse(Console.ReadLine());
+            int brMarkers = int.Parse(Console.ReadLine());
+            int prep = int.Parse(Console.ReadLine());
+            double percent = double.Parse(Console.ReadLine());
+
+            double pacPens = 5.8;
+            double pacMarkers = 7.2;
+            double prepLit = 1.2;
+
+            double sumPens = brPens * pacPens;
+            double sumMarkers = brMarkers * pacMarkers;
+            double sumPrep = prep * prepLit;
+            double realPerc = percent / 100;
+
+            double sum = sumPens + sumMarkers + sumPrep;
+            double sumPercent = sum - (sum * realPerc);
+
+            Console.WriteLine(sumPercent);
+
+        }
+    }
+}
