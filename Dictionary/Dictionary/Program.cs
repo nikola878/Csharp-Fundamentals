20fb5e1d406e14de1a525c1a1c7ffcf25ebb1808
Nikola Hadjiev <83541964+nikola878@users.noreply.github.com>
2022-05-21 18:03:37 +0300

    Add files via upload

---
+++ Dictionary/Dictionary/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> dictionary = Console.ReadLine()
+                .Split(" | ")
+                .ToList();
+            Dictionary<string, string> notebook = new Dictionary<string, string>();
+            foreach (string item in dictionary)
+            {
+                string[] tokens = item.Split(": ");
+                if (notebook.ContainsKey(tokens[0]))
+                {
+                    
+                }
+                else
+                {
+                    notebook.Add(tokens[0], tokens[1]);
+                }
+            }
+
+            List<string> words = Console.ReadLine().Split(" | ").ToList();
+
+            string command = Console.ReadLine();
+            if (command == "Test")
+            {
+                foreach (string word in words)
+                {
+                    foreach (var item in notebook)
+                    {
+                        if (notebook.ContainsKey(word))
+                        {
+                            Console.WriteLine($"{item.Key}:");
+                            Console.WriteLine($" -{item.Value}");
+                        }
+                    }
+                }
+            }
+            else if (command == "Hand Over")
+            {
+                foreach (var item in notebook)
+                {
+                    Console.Write(item.Key + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
