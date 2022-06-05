3a47bfe2e20c880cb1cb39efe4b20932c262c68f
Nikola Hadjiev <83541964+nikola878@users.noreply.github.com>
2022-06-05 16:58:37 +0300

    Add files via upload

---
+++ Christmas gifts/Christmas gifts/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Christmas_gifts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int kidsCount = 0;
+            int adultCount = 0;
+            int toysPrice = 0;
+            int sweatersPrice = 0;
+
+            string input = Console.ReadLine();
+            while (input != "Christmas")
+            {
+                int age = int.Parse(input);
+
+                if (age <= 16)
+                {
+                    kidsCount++;
+                    toysPrice += 5;
+                }
+                else if (age > 16)
+                {
+                    adultCount++;
+                    sweatersPrice += 15;
+                }
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Number of adults: {adultCount}");
+            Console.WriteLine($"Number of kids: {kidsCount}");
+            Console.WriteLine($"Money for toys: {toysPrice}");
+            Console.WriteLine($"Money for sweaters: {sweatersPrice}");
+        }
+    }
+}
